1bf05c0affcbd0a85de522699d1cd92d66e2755a
Justin W. Nicholson <36605158+JustinWNicholson@users.noreply.github.com>
2020-11-10 21:02:50 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# APP-IRATE
-Apprenticeship learning for IR
+# Apprenticeship learning using Inverse Reinforcement Learning
+
+## Introduction 
+Reinforcement learning (RL), often referred to as learning by exploration, that is by taking random actions initially and then slowly figuring out the actions which lead to the desired motion. In any RL setting, to make the agent perform different behaviors, it is the reward structure that one must modify/exploit. But assume we only have the knowledge of the behavior of the expert with us, then how do we estimate the reward structure given a particular behavior in the environment? This is the problem of Inverse Reinforcement Learning (IRL), where given the optimal/sub-optimal expert policy, we wish to determine the underlying reward structure.
+
+## Setup 
+In our case, we assume the actions taken by the humans are sub-optimal and we need the RL agent to take better actions. Apprenticeship Learning via IRL will try to understand the goal of an expert. It will learn a utility function (equivalently, a reward function) from observational data. We can then use the proposed utility function for standard reinforcement learning: asking an agent to explore to determine the best way to maximize the utility function.
+
+## How to Run
+- Run the ```irl_main.py``` file. It contains the main code in which DQN and all the other necessary modules have been called. Once the DQN policy has been trained enough (met the condition of IRL) it will return a ```model.h5``` file which will be our trained policy. We can then load this policy and execute in our environment to calculate the mean and SD of the new reward values we are getting. 
+- You also might want to change the ```TRAINING_FRAMES``` variable in ```DQN.py``` and ```NUM_OF_EPOCHS``` in ```simulator.py``` file. 
+- At the end you will be having a ```model.h5``` which is your trained policy and ```weights-optimal.txt``` file which is the set of weights for the most recent reward function. 
+- Once trained, run the ```test_play.py``` file to generate some rollouts on the trained policy using the most recent set of weights. This file generates rollouts for 2000 timesteps and calculates mean reward and standard deviation of the rewards along with feature expectation vector. 
+
+
+
+## Agent
+An agent is the algorithm which will make the decisions given a set of observations. 
+
+## Sensors
+An agent will be equipped with sensing capabilities to collect raw data (409 in our case). 
+
+## State Space
+The state of the agent consists of 409 observable features.
+
+## Rewards
+The reward after every decision is calculated as a weighted linear combination of the feature values observed in that frame. Here the reward rt in the tth frame, is calculated by the dot product of the weight vector w with the vector of feature values in tth frame, that is the state vector φt. Such that ```rt =wT ∗φt```.
+
+## Inverse RL
+- The features or the **basis functions** ```φi``` which are basically observable in the state. We define ```φ(st)``` to be the sum of all the feature expectations ```φi``` such that: ```phi(st) = φ1 + φ2 + ......φn```.
+- **Rewards rt** - Linear combination of these feature values observed at each state st. ```r(s,a,s′) = w1φ1 +w2φ2 +...+wnφn = wT ∗φ(st)```.
+- ***Feature expectations μ(π)*** of a policy π is the sum of discounted feature values φ(st). ```μ(π) = 􏰂∞t=0 γtφ(st).```
+The feature expectations of a policy are independent of weights, they only depend on the state visited during the run (according to the policy) and on the discount factor γ a number between 0 and 1 (e.g. 0.9 in our case). To obtain the feature expectations of a policy we have to execute the policy in real time with the agent and record the states visited and the feature values obtained.
+
+- ***Initialization*** 
+  - Expert policy feature expectations or the expert’s feature expectations ```μ(πE)``` are obtained by the actions that are taken according to the expert behavior. We basically execute this policy and get the feature expectations as we do with any other policy. The expert feature expectations are given to the IRL algorithm to find the weights such that the reward funciton corresponding to the weights resemebles the underlying reward function that the expert is trying to maximize (in usual RL language).
+  - Random policy feature expectations - execute a random policy and use the feature expectations obtained to initialize IRL.
+
+
+![Algorithm](ss.png)
---
+++ test_play.py
@@ -0,0 +1,182 @@
+import numpy as np 
+import pandas as pd 
+import daytime
+import re 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt 
+from collections import deque
+import random
+from keras.models import load_model
+import data_process
+
+
+####################################################
+########## IMPORT THE PROCESSED DATA ###############
+####################################################
+newdf = data_process.data_processing()
+
+def create_data(newdf):
+	# This simulator basically takes input as a state s and action a and spits out the next state s' i.e the new state is 'a' is taken in state 's'. 
+	print("Creating the (s, a, r, s') pairs...")
+	ls = []
+	l = len(newdf)
+	for index, row in newdf.iterrows():
+	    if index!= l-1:
+	        ls.append(newdf.iloc[index+1, 0])
+	    else:
+	        break
+	newdf.drop(newdf.tail(1).index,inplace=True)
+	newdf['next_state'] = ls
+	print("Printing the new dataframe with (s, a, r, s') trajectories...")
+	print("----------")
+	print(newdf.columns)
+	return newdf
+
+newdf = create_data(newdf)
+
+
+
+
+##################################################################
+################ CREATING THE ENVIRONMENT CLASS ##################
+##################################################################
+class Environment:
+
+	def __init__(self, transition_model):
+		self.transition_model = transition_model
+
+
+	def step(self, state, action, wt):
+		self.current_state = state
+		self.current_action = action 
+		self.W = wt
+
+		if isinstance(self.current_state, list):
+			self.current_state = self.current_state
+		else:
+			self.current_state = list(self.current_state)
+
+		if isinstance(self.current_action, list):
+			self.current_action = self.current_action
+		else:
+			self.current_action = list(self.current_action)
+
+		stack_ = [self.current_state, self.current_action]
+		stack_ = sum(stack_, [])
+# 		stack_ = reduce(operator.concat, stack_)
+		stack_ = np.asarray(stack_)
+		a = [stack_]
+		a = np.asarray(a)
+		self.next_state = self.transition_model.predict(a)
+		self.reward = np.sum(np.multiply(self.W, self.next_state.tolist()[0]), axis=0)
+		return self.next_state[0], self.reward
+
+
+
+def load_trained_weights(feature_weights):
+	#load the weights for reward function
+	f = open(feature_weights, 'r')
+	if f.mode == 'r':
+		trained_weights = f.read()
+
+	trained_weights = trained_weights.split()
+	tw = []
+	print(len(trained_weights))
+	for i in range(1,len(trained_weights)):
+		print(i)
+		if i == (len(trained_weights)-1):
+			temp = trained_weights[i]
+			temp = temp.replace(']', '')
+			print(temp)
+			tw.append(float(temp))
+		else:
+			tw.append(float(trained_weights[i]))
+
+	return tw
+
+
+def test_play(model_name, env, newdf, feature_weights):
+	GAMMA = 0.9
+	count=0
+
+	#load the trained model 
+	trained_model = load_model(model_name)
+
+	#load trained weights
+	trained_weights = load_trained_weights(feature_weights)
+
+	#start with a random initial state
+	state = random.choice(newdf.state) 
+	featureExpectations = np.zeros(len(trained_weights))
+	reward_list = []
+	while True:
+		count += 1
+
+		# Choose an action
+		action = trained_model.predict(np.asarray([np.asarray(state)]))[0]
+		print("old state:", state)
+		next_state, reward = env.step(state, action, trained_weights)
+		print("new state", next_state)
+		print("***********")
+		if count > 100:
+			featureExpectations += (GAMMA**(count-100))*next_state
+			reward_list.append(reward)
+
+		if count % 2000 == 0:
+			print("Ending the trajectory...")
+			mean_reward = np.mean(reward_list)
+			SD_reward = np.std(reward_list)
+			break 
+		state = next_state
+
+
+	return featureExpectations, mean_reward, SD_reward
+
+
+
+if __name__ == '__main__':
+
+
+	# load the transition model
+	transition_model = load_model('transition_model.h5')
+
+	# Create the environment
+	env = Environment(transition_model)
+
+	print("###########################################")
+	print("### STARTING TO TEST THE TRAINED POLICY ###")
+	print("###########################################")
+	print(" ")
+	print(" ")
+	print(" *** This policy will return featureExpectations of the new trained policy along with mean and standard deviation of reward values for a trajectory of length 2000 ***")
+	FE, mean_reward, SD_reward = test_play('model.h5', env, newdf, 'weights-optimal.txt')
+	print(" ")
+	print("The feature expectation of trained policy is: ", FE)
+	print(" ")
+	print("Mean reward obtained by this policy over a trajectory of 2000 steps is %s and SD is %s"%(mean_reward, SD_reward))
+	print(" ")
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
